7feb721a7fe7a4ae2c20579610ec438178de7715
Carlos Rivera <me@carlosrivera.im>
2021-03-04 13:18:18 -0600

    fixed failing test fixture

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -45,7 +45,7 @@ describe("convert()", () => {
 
   // this will test a file load from web, also the format is YAML instead JSON, and will create examples
   it("should execute from url", () => {
-    return expect(convert(urlThatExists, { outPath: "./build", snippetTargets: ["node", "python"] })).rejects.toBeDefined();
+    return expect(convert(urlThatExists, { outPath: "./build", snippetTargets: ["node", "python"] })).resolves.toBeUndefined();
   });
 
   // this will fail to load a file from web
